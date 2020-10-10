0b1e9ee93a00ade1441338ca939b0f5810752c34
Hootan Shadmehr <hootan@umich.edu>
2020-10-10 17:50:40 -0400

    Add more to .gitignore

--- babel.config.js
+++ babel.config.js
@@ -3,17 +3,7 @@ module.exports = {
   plugins: [
     '@babel/transform-runtime',
     '@babel/plugin-transform-strict-mode',
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true
-      }
-    ],
-    [
-      '@babel/plugin-proposal-private-methods',
-      {
-        loose: true
-      }
-    ]
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }]
   ]
 };
